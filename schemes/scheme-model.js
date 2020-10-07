b65377dcca5ba6b08e5dda1a213cd6e5b8510681
David Gold <davidgold@hey.com>
2020-10-07 18:13:46 -0400

    mvp met

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -2,12 +2,33 @@ const db = require("../data/db-config");
 
 module.exports = {
   find,
-  //   findById,
-  //   findSteps,
-  //   add,
-  //   remove,
+  findById,
+  findSteps,
+  add,
+  remove,
 };
 
 function find() {
   return db("schemes");
+}
+
+function findById(id) {
+  return db("schemes").where({ id }).first();
+}
+
+function findSteps(id) {
+  return db("steps").where({ id }).first();
+}
+
+function add(data) {
+  return db("schemes")
+    .insert(data, "id")
+    .then((ids) => {
+      const id = ids[0];
+      return findById(id);
+    });
+}
+
+function remove(id) {
+  return db("schemes").where({ id }).del();
 }
